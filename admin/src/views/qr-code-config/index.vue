4bfb12d4d91fcdc7e3808b0801bc6a9c7a64ba75
yanqian888-lab <yanqian888@icloud.com>
2026-08-31 12:03:04 +0800

    fix(admin): 二维码管理保存按钮无响应
    - 给保存/刷新按钮添加 native-type=button，防止在 el-form 内触发表单默认提交
    - 表单添加 @submit.prevent 作为兜底

--- admin/src/views/qr-code-config/index.vue
+++ admin/src/views/qr-code-config/index.vue
@@ -6,7 +6,7 @@
         上传小程序二维码/宣传二维码图片。上线后可在本页替换为正式小程序码，前端分享海报等位置会从这里读取最新图片。
       </p>
 
-      <el-form label-width="120px">
+      <el-form label-width="120px" @submit.prevent>
         <el-form-item label="二维码图片">
           <ImageUpload v-model="mpQrcodeUrl" width="160px" height="160px" tip="建议尺寸 400×400，PNG/JPG" />
         </el-form-item>
@@ -16,8 +16,8 @@
         </el-form-item>
 
         <el-form-item>
-          <el-button type="primary" :loading="saving" @click="save" v-perm="'app_config:write'">保存</el-button>
-          <el-button @click="load">刷新</el-button>
+          <el-button type="primary" native-type="button" :loading="saving" @click="save" v-perm="'app_config:write'">保存</el-button>
+          <el-button native-type="button" @click="load">刷新</el-button>
         </el-form-item>
       </el-form>
     </div>
